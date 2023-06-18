16d9b9bfcd3c017b5033e752593f7e9820a902bf
rskeans <ryan.skeans@sscinc.com>
2023-06-17 20:35:40 -0700

    Initial Commit

---
+++ stock-app/stock_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  stock_appApp.swift
+//  stock-app
+//
+//  Created by Ryan Skeans on 6/17/23.
+//
+
+import SwiftUI
+
+@main
+struct stock_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
